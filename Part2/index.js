92a88c8aacaec1220886da1c0c7f02b362647651
Christian Anderson <ca96187@gmail.com>
2019-03-25 18:23:54 -0600

    updated JS code

--- Part2/index.js
+++ Part2/index.js
@@ -9,6 +9,13 @@ let setCard = () => {
   card.style.color = colorInput.value;
 }
 
+// resets each card individually
 let resetCards = () => {
-
+  const card = document.getElementById(idInput.value);
+  card.style.color = "";
 }
+
+//can we reset all cards with a single button push?
+// for (let i = 0; i < .length; i++) {
+//   .color = "";
+// }
